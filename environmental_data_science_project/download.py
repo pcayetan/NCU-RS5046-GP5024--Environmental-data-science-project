3a767f0cccea39ab9401110e59432ccf720bcdff
pcayetan <po.cayetanot@gmail.com>
2025-05-22 12:43:42 +0800

    Removed test files

--- environmental_data_science_project/download.py
+++
@@ -1,8 +0,0 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-import kaggle
-
-api = KaggleApi()
-api.authenticate()
-
-kaggle.api.dataset_download_files('picekl/geoplant', path='./data/geoplant', unzip=True)
-
